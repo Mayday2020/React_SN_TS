fcb2c08f6a527b7e67f08327c87a0419f1367a98
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-05-26 23:42:14 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <div className="content-image">Image</div>
+            <div> ava + description</div>
+            <div>
+                my posts
+                <div>New post</div>
+                <div>
+                    Posts
+                    <div>post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </main>
+    )
+}
+export default Profile;
