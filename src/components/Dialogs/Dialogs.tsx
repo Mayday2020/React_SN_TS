7dea2f7318497e12ca6f4a58763a4baeb21949df
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-06-04 21:44:45 +0300

    'less21ts'

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,7 +5,20 @@ const Dialogs = () => {
     return (
         <main>
             <div className={s.content_image}>Dialogs Image</div>
-
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>Alex</div>
+                    <div className={s.dialog + ' ' + s.active}>Bob</div>
+                    <div className={s.dialog}>Ashley</div>
+                    <div className={s.dialog}>Victory</div>
+                    <div className={s.dialog}>Helen</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Is that u, Alex Clare?</div>
+                    <div className={s.message}>Boooob?! Do somethink!!!</div>
+                    <div className={s.message}>Not of your business</div>
+                </div>
+            </div>
         </main>
     )
 }
