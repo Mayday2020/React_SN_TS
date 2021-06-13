7641145b150197f4d9a7cf8abe821ce5cae6ce6c
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-06-13 14:39:50 +0300

    'less24ts'

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,6 +3,7 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 type MessageType = {
+    id: number
     message: string
 }
 type DialogItemType = {
@@ -24,21 +25,37 @@ const Message = (props: MessageType) => {
     )
 }
 const Dialogs = () => {
+    let dialogsData = [
+        {id: 1, name: 'Bob'},
+        {id: 2, name: 'Alex'},
+        {id: 3, name: 'Ashley'},
+        {id: 4, name: 'Victory'},
+        {id: 5, name: 'Helen'}
+    ],
+        messagesData = [
+            {id: 1, message: 'Is that u, Alex Clare?'},
+            {id: 1, message: 'Boooob?! Do somethink!!!'},
+            {id: 1, message: 'Not of your business'},
+            {id: 1, message: 'Not of your business'},
+            {id: 1, message: 'Not of your business'}
+        ]
     return (
         <main>
             <div className={s.content_image}>Dialogs Image</div>
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <DialogItem name={'Bob'} id={1}/>
-                    <DialogItem name={'Alex'} id={2}/>
-                    <DialogItem name={'Ashley'} id={3}/>
-                    <DialogItem name={'Victory'} id={4}/>
-                    <DialogItem name={'Helen'} id={5}/>
+                    <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+                    <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
+                    <DialogItem id={dialogsData[2].id} name={dialogsData[2].name} />
+                    <DialogItem id={dialogsData[3].id} name={dialogsData[3].name} />
+                    <DialogItem id={dialogsData[4].id} name={dialogsData[4].name} />
                 </div>
                 <div className={s.messages}>
-                    <Message message={'Is that u, Alex Clare?'}/>
-                    <Message message={'Boooob?! Do somethink!!!'}/>
-                    <Message message={'Not of your business'}/>
+                    <Message id={messagesData[0].id} message={messagesData[0].message}/>
+                    <Message id={messagesData[1].id} message={messagesData[1].message}/>
+                    <Message id={messagesData[2].id} message={messagesData[2].message}/>
+                    <Message id={messagesData[3].id} message={messagesData[3].message}/>
+                    <Message id={messagesData[4].id} message={messagesData[4].message}/>
                 </div>
             </div>
         </main>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from './MyPosts.module.css';
 import Post from "./Posts/Post";
 
 const MyPosts = () => {
+    let postData = [
+        { id: 1, message: "Hi, how are you?", likesCount: 30 },
+        { id: 2, message: "It's my first post.", likesCount: 17 },
+        { id: 3, message: "Please, like this comment))", likesCount: 91 }
+    ]
     return (
         <div className={s.item}>
             <div className={s.newPost}>
@@ -12,9 +17,9 @@ const MyPosts = () => {
             </div>
             <div className={s.posts}>
                 Posts
-                <Post message="Hi, how are you?" likesCount={30}/>
-                <Post message="It's my first post." likesCount={17}/>
-                <Post message="Please, like this comment))" likesCount={91}/>
+                <Post id={postData[0].id} message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post id={postData[1].id} message={postData[1].message} likesCount={postData[1].likesCount}/>
+                <Post id={postData[2].id} message={postData[2].message} likesCount={postData[2].likesCount}/>
             </div>
         </div>
     )
